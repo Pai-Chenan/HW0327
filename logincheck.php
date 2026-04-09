6987f65b1588a7b49192e3c51e8988b2bea59037
Pai-Chenan <a1123359@mail.nuk.edu.tw>
2026-04-10 00:58:51 +0800

    Update logincheck.php

--- logincheck.php
+++ logincheck.php
@@ -1,22 +1,17 @@
 <?php
-session_start();
 $fID = "user";
 $fPWD = "0502";
 
-if(isset($_POST["uID"]) && isset($_POST["uPWD"])){
+if (isset($_POST["uID"]) && isset($_POST["uPWD"])) {
     $uID = $_POST["uID"];
     $uPWD = $_POST["uPWD"];
 
-    if($fID == $uID && $fPWD == $uPWD){
+    if ($fID == $uID && $fPWD == $uPWD) {
         $_SESSION["login_status"] = "success";
         header("Location: form.php");
-        exit(); 
-    } else {
-        header("Location: login.php?error=1");
         exit();
+    } else {
+        $error_message = "登入失敗！帳號或密碼錯誤。";
     }
-} else {
-    header("Location: login.php");
-    exit();
 }
 ?>
