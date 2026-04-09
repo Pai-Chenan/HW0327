6d5a02d38ec98a68d88131a61ad68ca8ad006bc7
Pai-Chenan <a1123359@mail.nuk.edu.tw>
2026-04-10 00:59:47 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,26 @@
+<?php
+session_start();
+$error_message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require("logincheck.php"); 
+}
+    
+?>
 <html>
 <head><title>夏令營登入頁面</title></head>
 <body>
 <center>
     <h1>登入</h1>
 
-    <?php 
-    if (isset($_GET["error"]) && $_GET["error"] == 1) {
-        echo "<p style='color:red; font-weight:bold;'>登入失敗！帳號或密碼錯誤。</p>";
-    }
-    ?>
+    <?php if ($error_message !== ""): ?>
+        <p style="color:red; font-weight:bold;"><?php echo $error_message; ?></p>
+    <?php endif; 
+?>
     
-    <form action="logincheck.php" method="POST">
-        帳號：<input type="text" name="uID" required></br>
-        密碼：<input type="password" name="uPWD" required></br>
+    <form action="login.php" method="POST">
+        帳號：<input type="text" name="uID" required></br></br>
+        密碼：<input type="password" name="uPWD" required></br></br>
         <input type="submit" value="登入">
         <input type="reset" value="清除">
     </form>
